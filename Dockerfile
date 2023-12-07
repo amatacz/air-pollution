16d37023277e8bc54eef415f73847d7aaab23af1
Aleksandra <aleksandra.matacz93@gmail.com>
2023-12-07 10:51:45 +0100

    changes

--- Dockerfile
+++ Dockerfile
@@ -13,21 +13,6 @@ WORKDIR $WORK_DIRECTORY
 
 COPY . ./
 
-# RUN pwd && ls
-#
-# CMD python ./gcloud/gcloud_data_pipeline/gcp_scripts/upload_dags_to_composer.py \
-#         --dags_directory=./gcloud/gcloud_data_pipeline/dags/ \
-#         --dags_bucket=air_pollution_composer_bucket_amatacz \
-#         --bucket_folder_name=dags && \
-#     python ./gcloud/gcloud_data_pipeline/gcp_scripts/upload_dags_to_composer.py \
-#         --dags_directory=./gcloud/gcloud_data_pipeline/plugins/ \
-#         --dags_bucket=air_pollution_composer_bucket_amatacz \
-#         --bucket_folder_name=dags/plugins && \
-#     python ./gcloud/gcloud_data_pipeline/gcp_scripts/upload_dags_to_composer.py \
-#         --dags_directory=./gcloud/gcloud_data_pipeline/pyspark/ \
-#         --dags_bucket=air_pollution_composer_bucket_amatacz \
-#         --bucket_folder_name=pyspark 
-
 CMD python $WORK_DIRECTORY/gcp_scripts/upload_dags_to_composer.py \
         --dags_directory=$WORK_DIRECTORY/dags/ \
         --dags_bucket=air_pollution_composer_bucket_amatacz \
--- main.py
+++ main.py
@@ -1,66 +1,20 @@
-<<<<<<< HEAD
-# from cloud_functions import cloud_integration
-# from etl import (
-#     data_ingestion,
-#     data_configuration,
-#     data_transformation
-# )
-=======
 from cloud_functions import cloud_integration
 from etl import (
     data_ingestion,
     data_configuration,
     data_transformation
 )
->>>>>>> b4a6d4a96998c08c8d0601c25df1885f34892e20
 
-# if __name__ == "__main__":
-#     # create data configurator object
-#     DataConfiguratorObject = data_configuration.DataConfigurator()
+if __name__ == "__main__":
+    # create data configurator object
+    DataConfiguratorObject = data_configuration.DataConfigurator()
 
-#     # create cloud configurator object
-#     CloudIntegratorObject = cloud_integration.CloudIntegration()
+    # create cloud configurator object
+    CloudIntegratorObject = cloud_integration.CloudIntegration()
 
-#     # create data extraction object
-#     OpenWeatherDataIngestorObject = data_ingestion.OpenWeatherDataIngestor()
+    # create data extraction object
+    OpenWeatherDataIngestorObject = data_ingestion.OpenWeatherDataIngestor()
 
-<<<<<<< HEAD
-#     # create data transformator object
-#     DataTransformatorObject = data_transformation.OpenWeatherHistoricalDataTransformator()
-
-#     # load cities from yaml file
-#     cities = DataConfiguratorObject.load_cities_from_yaml()
-
-#     # ingest all city data and return all_city_data dictionary
-#     all_city_data = OpenWeatherDataIngestorObject.all_city_data_ingest(cities, 1696320000, 1696356000)
-
-#     # upload to cloud bucket
-#     CloudIntegratorObject.upload_data_to_cloud_from_dict('air_pollution_bucket_amatacz', all_city_data, 'all_city_data.json')
-
-#     # download data from bucket
-#     CloudIntegratorObject.download_data_from_cloud_to_dict('air_pollution_bucket_amatacz', 'all_city_data.json')
-
-#     # get and transform historic air pollution data for all cities
-#     all_city_data_frame = DataTransformatorObject.historic_data_transform(all_city_data)
-
-#     # melt all_city_data_frame
-#     all_city_data_melted = DataTransformatorObject.melt_all_cities_data_frame(all_city_data_frame)
-
-#     ''' Run a loop through all cities in dataframe, create table and populate it with data. '''
-#     # get schema for city table 
-#     city_table_schema = DataConfiguratorObject.load_city_table_schema_from_yaml()
-#     for city in all_city_data_frame['city'].unique():
-#         # create dataframe with city data
-#         city_data_frame = DataTransformatorObject.save_city_data_to_city_dataframe(city, all_city_data_frame)
-#         # create dataset if not exists, create city table and populate it with data
-#         CloudIntegratorObject.create_dataset_table_and_insert_data("air_pollution", city, city_table_schema, city_data_frame)
-
-#     ''' Create dataset and table for unified city data and populate it with data.'''
-#     # get schema for unified city table
-#     unified_city_table_schema = DataConfiguratorObject.load_unified_city_table_schema_from_yaml()
-#     # create dataset if not exists, create city table and populate it with data
-#     CloudIntegratorObject.create_dataset_table_and_insert_data("air_pollution_dataset_unified", "unified_city_data", unified_city_table_schema, all_city_data_melted)
-=======
     # create data transformator object
     DataTransformatorObject = data_transformation.OpenWeatherHistoricalDataTransformator()
 
@@ -96,4 +50,3 @@ from etl import (
     unified_city_table_schema = DataConfiguratorObject.load_unified_city_table_schema_from_yaml()
     # create dataset if not exists, create city table and populate it with data
     CloudIntegratorObject.create_dataset_table_and_insert_data("air_pollution_dataset_unified", "unified_city_data", unified_city_table_schema, all_city_data_melted)
->>>>>>> b4a6d4a96998c08c8d0601c25df1885f34892e20
